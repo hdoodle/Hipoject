ce1a95a88b93b6b744e4bbcb06f6004ff3eb9707
GunmetalOrb73 <48606298+GunmetalOrb73@users.noreply.github.com>
2020-01-20 15:46:12 -0500

    Update config.php

--- config.php
+++ config.php
@@ -1,25 +1,8 @@
-<!---copyright (c) 2013-2013 hehaoyuan1997 All Rights Reserved--->
-<!---Powered By hehaoyuan1997--->
-<!---author website:http://www.ddpool.com/--->
-<!---hehaoyuan1997开始于2013年8月18日23:19:57编写--->
-
-<!---全局配置文件--->
-<?php
-$websitename="Hipoject";                   //网站名称
-$websiteurl="http://127.0.0.1/";   //网站链接，注意：末尾一定要有"/"
-$version="0.6";                    
-$build="200120469";
-$date="1579392000";
-$copyright="copyright (c) 2013-2014 hehaoyuan1997 All Rights Reserved";
-$imagefile="files/"                        //建议不要更改此项
-?>
-
-
 <?php
 $websitename="Hipoject";            //Websitename      
 $websiteurl="http://127.0.0.1/";  //Website Url, note: at the end must have a "/"
 $version="1.0";                    
-$build="2014061400102";
+$build="200120469";
 $date="1579392000";
 
 $imagefile="files/"            //It is not recommended to change this item            
